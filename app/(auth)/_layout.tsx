c2417d50a204941ab25ccb11601786bcf4a39416
byiringiro215 <emmabyiringiro215@gmail.com>
2025-07-20 13:55:56 +0200

    Creating basic home page and folder structure for the rest of the app

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Slot } from 'expo-router'
+
+export default function _layout() {
+  return (
+    <SafeAreaView>
+    <View>
+      <Text>Auth _layout </Text>
+    </View>
+    <Slot/>
+    </SafeAreaView>
+  )
+}
